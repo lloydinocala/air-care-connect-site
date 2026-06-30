0f3cb287b08d2f8e80926f96fa1d39fcb51554a1
lloydinocala <lloyd@dynamicair-care.com>
2026-06-29 23:28:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Club from './components/Club';
 import About from './components/About';
 import Contact from './components/Contact';
 import AireAzul from './components/AireAzul';
+import ServicesEs from './components/ServicesEs';
+import ClubEs from './components/ClubEs';
+import AboutEs from './components/AboutEs';
+import ContactEs from './components/ContactEs';
 import ClubDocuments from './components/ClubDocuments';
 import ClubDocumentsEs from './components/ClubDocumentsEs';
 import ComfortGuideWidget from './components/ComfortGuideWidget';
@@ -22,14 +26,21 @@ export default function App() {
         <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <main onClick={() => setMenuOpen(false)}>
           <Routes>
+            {/* English */}
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Services />} />
             <Route path="/club" element={<Club />} />
             <Route path="/club/documents" element={<ClubDocuments />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+
+            {/* Español */}
             <Route path="/aire-azul" element={<AireAzul />} />
+            <Route path="/aire-azul/services" element={<ServicesEs />} />
+            <Route path="/aire-azul/club" element={<ClubEs />} />
             <Route path="/aire-azul/documentos" element={<ClubDocumentsEs />} />
+            <Route path="/aire-azul/about" element={<AboutEs />} />
+            <Route path="/aire-azul/contact" element={<ContactEs />} />
           </Routes>
         </main>
         <Footer />
